75aacca4f5d65e5a5dd09bcf12544c49eb9d68b9
snytav <snytav@gmail.com>
2025-03-26 15:33:38 +0200

    cosine with 2n param  -  OK

--- m2D_optim.m
+++ m2D_optim.m
@@ -2,14 +2,14 @@ rng default % for reproducibility
 tdata = 0:1:10;
 xdata = 0:1:10;
 [X,T] = meshgrid(xdata,tdata)
-ydata = 0.5*cos(X); %*cos(b*T));% + randn(size(tdata,2),size(xdata,2));
+ydata = cos(X*0.8); %*cos(b*T));% + randn(size(tdata,2),size(xdata,2));
 figure;
 surf(X,T,ydata);
 
 
 
 
-fun = @(x)norm(ydata - x(1)*cos(X) );
+fun = @(x)norm(ydata - cos(X*x(2)) );
 x0 = [-1.2,1];
 x = fminsearch(fun,x0)
 qq = 0;
