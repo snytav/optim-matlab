50069ed5d1efbc7f07b1c3296799c87104413844
snytav <snytav@gmail.com>
2025-03-26 15:24:19 +0200

    cosine with 2 params  - not OK

--- m2D_optim.m
+++ m2D_optim.m
@@ -2,14 +2,14 @@ rng default % for reproducibility
 tdata = 0:1:10;
 xdata = 0:1:10;
 [X,T] = meshgrid(xdata,tdata)
-ydata = cos(X); %*cos(b*T));% + randn(size(tdata,2),size(xdata,2));
+ydata = 0.5*cos(0.8*X); %*cos(b*T));% + randn(size(tdata,2),size(xdata,2));
 figure;
 surf(X,T,ydata);
 
 
 
 
-fun = @(x)norm(ydata - x(1)*cos(X) );
+fun = @(x)norm(ydata - x(1)*cos(x(2)*X) );
 x0 = [-1.2,1];
 x = fminsearch(fun,x0)
 qq = 0;
--- surf1.m
+++ surf1.m
@@ -5,6 +5,7 @@ z = rand(11,8);
 [xx,yy] = meshgrid(x,y);
 
 surf(xx,yy,z);
+scatter3(xx,yy,)
 
 x = 1:8;
 y = 1:11;
