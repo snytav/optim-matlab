c96f2bfb57e3228fd84c3a5f6d09904ce37d3ec5
snytav <snytav@gmail.com>
2025-03-26 15:55:48 +0200

    optimiztion plot

--- m2D_optim.m
+++ m2D_optim.m
@@ -2,14 +2,19 @@ rng default % for reproducibility
 tdata = 0:1:10;
 xdata = 0:1:10;
 [X,T] = meshgrid(xdata,tdata)
-ydata = cos(X*0.8); %*cos(b*T));% + randn(size(tdata,2),size(xdata,2));
+ydata = 0.5*cos(X*0.8); %*cos(b*T));% + randn(size(tdata,2),size(xdata,2));
 figure;
 surf(X,T,ydata);
 
 
 
 
-fun = @(x)norm(ydata - cos(X*x(2)) );
+fun = @(x)norm(ydata - x(1)*cos(X*x(2)) );
 x0 = [-1.2,1];
-x = fminsearch(fun,x0)
+
+%result gets worse but unevenly if you reduce FiniteDifferenceStepSize
+% opt = optimset('fminsearch', 'StepTolerance', 1e-14, 'Display', 'iter', 'FiniteDifferenceStepSize', 1e-9);
+options = optimset('PlotFcns',@optimplotfval);
+
+x = fminsearch(fun,x0,options);
 qq = 0;
